c2889a3fa451e50bcead56bb2521755380659edd
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-29 18:54:13 -0300

    refatorado

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
+const value1 = document.getElementById('value1');
+const value2 = document.getElementById('value2');
+
 // Requisito 1 - Lance um erro em caso de input vazio.
 const emptyInput = () => {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  if (!value1 || !value2) {
+  if (!value1.value || !value2.value) {
     throw new Error('Preencha os campos para realizar a soma');
   }
 };
+
 // Requisito 2 - Lance um erro em caso de valores não numéricos.
 const isNumber = () => {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
+  const result = Number(value1.value) + Number(value2.value);
   if (Number.isNaN(Number(result))) {
     throw new Error('Informe dois números para realizar a soma');
   }
 };
+
 // Requisito 3 - Apague os inputs ao clicar no botão de realizar a soma.
 const clearInput = () => {
-  const value1 = document.getElementById('value1');
-  const value2 = document.getElementById('value2');
   value1.value = '';
   value2.value = '';
 };
 
 function calculateSum() {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
   try {
     emptyInput();
     isNumber();
-    const result = Number(value1) + Number(value2);
+    const result = Number(value1.value) + Number(value2.value);
     return result;
   } catch (e) {
     return e.message;
