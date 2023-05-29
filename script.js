8fe36739081eb45529e114165db06c3fa7dde85b
Felipe Duarte <feduarte.dev@gmail.com>
2023-05-29 15:33:35 -0300

    primeiro requisito pronto

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+
+const verificaNumeros = () => {
+  const value1 = document.getElementById('value1').value;
+  const value2 = document.getElementById('value2').value;
+  if (!value1 || !value2) {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+};
+
 function calculateSum() {
   const value1 = document.getElementById('value1').value;
   const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
-
-  return result;
+  try {
+    verificaNumeros();
+    const result = Number(value1) + Number(value2);
+    return result;
+  } catch (error) {
+    return error.message;
+  }
 }
 
 function displayResult(result) {
